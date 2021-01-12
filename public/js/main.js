6242a7065faa207bb5ca8b87d211ad54b218a6d4
DawiT <Dawid@Tararuj.be>
2021-01-12 14:10:23 +0100

    exo 2 fin

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+/*
+// exo 1 
+let prenoms = ["Ayhan", "Zulma", "Adame", "Seb", "Malo","Max", "Max2","Alex","Natchez"]; 
+
+let i = 0; 
+
+
+//exo 1 methode 1 
+do{
+    console.log(prenoms[i]);
+    i++;
+}while(i < prenoms.length); 
+
+
+
+// exo 1 methode 2 
+while(i < prenoms.length){
+    console.log(prenoms[i]);
+    i++;
+}
+ 
+*/
+
+// exo 2 
+
+/*
+let fruits = ["babane", "pomme", "tomate", "cerise"]; 
+
+while(fruits.length > 0){
+    console.log(fruits);
+    fruits.shift(); // ou fruits.pop(); si supp de la fin ; 
+}
+
+console.log(fruits);
+
+*/
+
+//exo 3 
+
+
+
+
